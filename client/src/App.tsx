0c9114074ecbc050500501ce3b410929869ad179
luevrata <firsovalizzz@gmail.com>
2025-01-18 12:45:20 -0800

    set up fe

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MainPage from "./pages/MainPage.tsx";
+import { Box } from "@mui/material";
+const App = () => {
+  return (
+    <Box>
+      <MainPage />
+    </Box>
+  );
+};
+export default App;
---
+++ client/src/pages/MainPage.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Box } from "@mui/material";
+const App = () => {
+  return <Box></Box>;
+};
+export default App;
