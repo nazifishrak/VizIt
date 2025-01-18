dd238613a81f0465cff9e6bfb8d9b87e6daf214f
luevrata <firsovalizzz@gmail.com>
2025-01-18 13:14:36 -0800

    set prompt page

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,6 +1,52 @@
 import React from "react";
-import { Box } from "@mui/material";
-const App = () => {
-  return <Box></Box>;
+import { Box, Typography, TextField, Button, Paper } from "@mui/material";
+import DoubleArrowIcon from "@mui/icons-material/DoubleArrow";
+
+const MainPage: React.FC = () => {
+  return (
+    <Box
+    // sx={blackBoxStyle}
+    >
+      <Paper
+        elevation={8}
+        // sx={blackBoxStyle}
+      >
+        <Typography
+          variant="h4"
+          sx={{
+            marginBottom: 3,
+            fontWeight: "bold",
+            color: "#ffffff",
+            textShadow: "0px 2px 5px rgba(0, 0, 0, 0.5)",
+          }}
+        >
+          What do you want to get explained?
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            gap: 2,
+            alignItems: "center",
+            marginBottom: 2,
+          }}
+        >
+          <TextField
+            variant="outlined"
+            placeholder="Ask anything..."
+            value={prompt}
+            // sx={textFieldStyle}
+          />
+          <Button
+            variant="contained"
+            size="large"
+            // sx={goButtonStyle}
+          >
+            <DoubleArrowIcon sx={{ color: "black" }} />
+          </Button>
+        </Box>
+      </Paper>
+    </Box>
+  );
 };
-export default App;
+
+export default MainPage;
